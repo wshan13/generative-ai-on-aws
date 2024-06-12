25ba26119b847290298e64783d3650b09daf8180
aksen1312 <aksen1312@gmail.com>
2024-06-13 01:41:00 +0900

    Add files via upload

--- 05_finetune/scripts/utils/pack_dataset.py
+++ 05_finetune/scripts/utils/pack_dataset.py
@@ -2,38 +2,38 @@ from functools import partial
 from itertools import chain
 
 
+# 다음 배치에서 사용할 수 있도록 배치에서 남은 부분을 저장할 빈 리스트 생성
 remainder = {"input_ids": [], "attention_mask": [], "token_type_ids": []}
 
 
-# empty list to save remainder from batches to use in next batch
 def pack_dataset(dataset, chunk_length=2048):
     print(f"Chunking dataset into chunks of {chunk_length} tokens.")
 
     def chunk(sample, chunk_length=chunk_length):
-        # define global remainder variable to save remainder from batches to use in next batch
+        # 배치에서 남은 데이터를 다음 배치에서 재사용하기 위한 전역 변수 정의
         global remainder
-        # Concatenate all texts and add remainder from previous batch
+        # 모든 텍스트를 연결하고 이전 배치에서 남은 데이터를 추가
         concatenated_examples = {k: list(chain(*sample[k])) for k in sample.keys()}
         concatenated_examples = {k: remainder[k] + concatenated_examples[k] for k in concatenated_examples.keys()}
-        # get total number of tokens for batch
+        # 배치의 총 토큰 수 계산
         batch_total_length = len(concatenated_examples[list(sample.keys())[0]])
 
-        # get max number of chunks for batch
+        # 배치의 최대 청크 수 가져오기
         if batch_total_length >= chunk_length:
             batch_chunk_length = (batch_total_length // chunk_length) * chunk_length
 
-        # Split by chunks of max_len.
+        # max_len 크기로 청크 나누기
         result = {
             k: [t[i : i + chunk_length] for i in range(0, batch_chunk_length, chunk_length)]
             for k, t in concatenated_examples.items()
         }
-        # add remainder to global variable for next batch
+        # 남은 데이터를 다음 배치를 위한 전역 변수에 추가
         remainder = {k: concatenated_examples[k][batch_chunk_length:] for k in concatenated_examples.keys()}
-        # prepare labels
+        # 레이블 준비
         result["labels"] = result["input_ids"].copy()
         return result
 
-    # tokenize and chunk dataset
+    # 데이터세트를 토크나이즈하고 청크 나누기
     lm_dataset = dataset.map(
         partial(chunk, chunk_length=chunk_length),
         batched=True,
